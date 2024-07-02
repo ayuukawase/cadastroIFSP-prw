aa9281c453bab22fff95cd721b3c07079e160d16
ayumi <leticiakawase@gmail.com>
2024-07-02 12:16:08 -0300

    finished

--- includes/conexao.php
+++ includes/conexao.php
@@ -9,7 +9,7 @@
     $con = mysqli_connect($hostname, $username, $password, $database, $port);
 
     if(mysqli_connect_errno()) {
-        printf("Erro conexão: %s", mysqli_connect_errno());
+        printf("Erro conexão: %s", mysqli_connect_error());
         exit();
     }
 
